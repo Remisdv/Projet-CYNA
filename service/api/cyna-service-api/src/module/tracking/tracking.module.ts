d10b112dd39ec4b1414f69b5aebd7a904df04b63
RemiSDV <lhuillierremi1@gmail.com>
2026-04-16 11:31:30 +0200

    fix: correct import paths in tracking module

--- service/api/cyna-service-api/src/module/tracking/tracking.module.ts
+++ service/api/cyna-service-api/src/module/tracking/tracking.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TrackingEventEntity } from '../database/entity/tracking/tracking-event.entity';
-import { TrackingService } from '../service/tracking/tracking.service';
-import { TrackingController } from '../endpoint/tracking/tracking.controller';
+import { TrackingEventEntity } from '../../database/entity/tracking/tracking-event.entity';
+import { TrackingService } from '../../service/tracking/tracking.service';
+import { TrackingController } from '../../endpoint/tracking/tracking.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([TrackingEventEntity])],
